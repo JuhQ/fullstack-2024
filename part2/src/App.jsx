aee8ee121149e151f9bb2458685cb5bf0eb823cc
Juha Tauriainen <juha@bin.fi>
2024-03-19 19:50:51 +0200

    Add part2 examples

---
+++ part2/src/App.jsx
@@ -0,0 +1,112 @@
+import { useState, useEffect } from 'react'
+import Note from './components/Note'
+import Notification from './components/Notification'
+import noteService from './services/notes'
+
+const App = () => {
+  const [notes, setNotes] = useState(null)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(false)
+  const [error, setError] = useState("")
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(setNotes)
+  }, [])
+
+  const addNote = (event) => {
+    event.preventDefault()
+    setError("")
+
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5
+    }
+
+    noteService
+      .create(noteObject)
+      .then(() => {
+        setNotes(notes.concat(noteObject))
+        setNewNote('')
+      })
+      .catch((error) => setError(error.message))
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+    setError("")
+  }
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    setError("")
+
+    noteService
+      .update(2134, changedNote)
+      .then(data => {
+        setNotes(notes.map(note => note.id !== id ? note : data))
+      })
+      .catch(() => {
+        setNotes(notes.filter(n => n.id !== id))
+        setError(`the note '${note.content}' was already deleted from server`)
+      })
+  }
+
+  const deleteNote = (id) =>
+    noteService
+      .delete(id)
+      .then(() => {
+        setNotes(notes.filter(n => n.id !== id))
+      })
+
+
+  if (!notes) {
+    return null;
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
+
+  return (
+    <div>
+      <h1>Notes</h1>
+
+      {error.length > 0 && <Notification message={`Virhe tapahtui! ${error}`} />}
+
+      <div>
+        <button
+          type="button"
+          onClick={() => setShowAll(!showAll)}
+        >
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note =>
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() =>
+              toggleImportanceOf(note.id)
+            }
+            deleteNote={() => deleteNote(note.id)}
+          />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
---
+++ part2/src/services/notes.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/notes'
+
+const getResponseData = (response) => response.data
+
+const getAll = () =>
+    axios
+        .get(baseUrl)
+        .then(getResponseData)
+
+const create = (note) =>
+    axios
+        .post(baseUrl, note)
+        .then(getResponseData)
+
+const update = (id, note) =>
+    axios.put(`${baseUrl}/${id}`, note)
+        .then(getResponseData)
+
+const deleteNote = (id) =>
+    axios
+        .delete(`${baseUrl}/${id}`)
+        .then(getResponseData)
+
+export default {
+    getAll,
+    create,
+    update,
+    delete: deleteNote
+}
