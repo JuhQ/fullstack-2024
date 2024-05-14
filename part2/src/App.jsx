3ada9389821274aa4abc7f076baad5b6adf9ea4f
Juha Tauriainen <juha@bin.fi>
2024-05-14 19:43:56 +0300

    Login to frontend

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -2,13 +2,16 @@ import { useState, useEffect } from 'react'
 import Note from './components/Note'
 import Notification from './components/Notification'
 import noteService from './services/notes'
-import Country from './components/Country'
+import login from './services/login'
 
 const App = () => {
   const [notes, setNotes] = useState(null)
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(false)
   const [error, setError] = useState("")
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [user, setUser] = useState("")
 
   useEffect(() => {
     noteService
@@ -64,6 +67,27 @@ const App = () => {
       })
 
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log("log in", username, password)
+
+    try {
+      const loginResult = await login({ username, password })
+
+      setUser(loginResult)
+
+      const { token } = loginResult
+      console.log("token", token)
+      console.log("login result", loginResult)
+    } catch (error) {
+      setError(error.message)
+
+      setTimeout(() => {
+        setError("")
+      }, 5000)
+    }
+  }
+
   if (!notes) {
     return null;
   }
@@ -72,12 +96,54 @@ const App = () => {
     ? notes
     : notes.filter(note => note.important)
 
-  return <Country />
+
+  const loginForm = () => {
+    return (<>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+
+        <button type="submit">Log in</button>
+      </form>
+    </>)
+  }
+
+
+  const noteForm = () => {
+    return (
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>)
+  }
 
   return (
     <div>
 
-      <Country />
+      {!user && loginForm()}{user && <div>
+       <p>{user.username} logged in</p>
+         {noteForm()}
+      </div>
+    }
+
       <h1>Notes</h1>
 
       {error.length > 0 && <Notification message={`Virhe tapahtui! ${error}`} />}
@@ -102,13 +168,6 @@ const App = () => {
           />
         )}
       </ul>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
-      </form>
     </div>
   )
 }
